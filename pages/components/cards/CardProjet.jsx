6eedfb4342fa795f01c07fe31c2d21e37a71fe73
ElskaWitch <justinelabas95@gmail.com>
2022-10-27 11:42:53 +0200

    fix deploy

--- pages/components/cards/CardProjet.jsx
+++ pages/components/cards/CardProjet.jsx
@@ -7,7 +7,7 @@ export default function CardProjet({projet}) {
   return (
       <div className='max-w-sm shadow-lg p-4 grid justify-items-center rounded-xl'>
         <div>
-         {/* <img src={featuredImage.fields.file.url} alt={title} /> */}
+         <img src={featuredImage.fields.file.url} alt={title} />
         </div>
         <div>
          <p className='text-xl font-bold py-3 text-center'>{title}</p>
